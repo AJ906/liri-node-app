f6c967d5de82b31ffdb469419d7f9e925647ae04
Angela <johnson.angela.d@gmail.com>
2018-04-21 12:10:04 -0400

    basic structure w/JSON - no functions

--- keys.js
+++ keys.js
@@ -2,14 +2,14 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.reu9ejlfYEzfdjCkX7bcYiwTj,
+  consumer_secret: process.env.wp6AVJgBtS5pbmiaImwQtVUp6ZP9sO7hW0xqFP7B0ZbNdvTKAk,
+  //access_token_key: process.env.987019006459744256-9OsFj5cucGtsggufUJl7mDBo3PXD4MJ,
+  access_token_secret: process.env.r9vjn5kE3GxHMu153xr6hDS7rmP1NIzjESIiLiGaIIksX
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.f05a2750467641f4a104251a1858b7b7,
+  secret: process.env.c12406440b8847e695a76671b7d757d3
 };
 // ```
